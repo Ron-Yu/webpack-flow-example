182c7383aacde0cd5d65d33f24d69770e7a66c90
Ron Yu <ronyu77@gmail.com>
2016-03-29 21:53:27 +0800

    update home.js

--- build/js/home.js
+++ build/js/home.js
@@ -3,6 +3,8 @@ webpackJsonp([2],[
 /***/ function(module, exports, __webpack_require__) {
 
 	var say = __webpack_require__(1);
+
+	// this is commonjs async loading function, require([files], callback, chuckName), if the third parameter is missed, the output file name wil be ugly.
 	__webpack_require__.e/* nsure */(3, function (require) {
 	    var act = __webpack_require__(2);
 	    act();
